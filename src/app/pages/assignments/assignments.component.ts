949266bcd30014da2ec1ba3271084b848dcb7069
heyleyzz <lhsilubrico1893@liceo.edu.ph>
2026-04-17 02:06:48 +0800

    Animations

--- src/app/pages/assignments/assignments.component.ts
+++ src/app/pages/assignments/assignments.component.ts
@@ -66,7 +66,6 @@ export class AssignmentsComponent implements OnInit {
       isDone: false
     });
 
-    // RESET FORM
     this.newTask = {
       id: 0,
       title: '',
@@ -77,5 +76,10 @@ export class AssignmentsComponent implements OnInit {
     };
 
     this.showForm = null;
+  }
+
+  // 🔥 ADD THIS ONLY
+  deleteTask(id: number): void {
+    this.assignmentsService.removeTask(id);
   }
 }
